a64c88199f3d1c6ca1ab4eae3b54521e2cbac766
Patrick Pulwey <patrick.pulwey@cloudbucket.de>
2022-03-28 21:14:16 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import bodyParser from "body-parser";
+import express from "express";
+import helmet from "helmet";
+import yaml from "js-yaml";
+import fs from "fs-extra";
+import path from 'path';
+
+const configPath = path.join('config', 'deploy-server.yml')
+const { json } = bodyParser;
+const app = express();
+const config = yaml.load(fs.readFileSync(configPath, "utf8"));
+
+const { serverPort } = config;
+
+app.use(helmet());
+app.use(
+  json({
+    verify: (req, res, buf, encoding) => {
+      if (buf && buf.length) {
+        req.rawBody = buf.toString(encoding || "utf8");
+      }
+    },
+  })
+);
+
+app.post('/', (req, res) => {
+  res.send('Success!');
+});
+
+app.listen(serverPort, () => {
+  console.log(`Listening on ${serverPort}`);
+});
